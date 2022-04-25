398bd22c32b66f38ea3597f367045b7da7619560
mhannani <mohamed.hanani2019@gmail.com>
2022-04-25 09:26:05 -0400

    Experiments

--- setup.sh
+++ setup.sh
@@ -4,14 +4,14 @@ echo "Cloning project into anti-spoofing-rPPG"
 
 git clone "https://github.com/mhannani/anti-spoofing-rPPG"
 
-echo "getting into anti-spoofing-rPPG"
+echo "getting into anti_spoofing_rPPG"
 cd anti-spoofing-rPPG
 
 echo "Creating conda environment..."
-conda create --name anti-spoofing-rPPG_1
+conda create --name anti_spoofing_rPPG
 
 echo "activating environment..."
-conda activate anti-spoofing-rPPG_1
+conda activate anti_spoofing_rPPG
 
 echo "Installing dependencies..."
 conda install --file requirements.txt
