a722a105971f8da87a8c6ffb6b02c6cec8beba94
mhannani <mohamed.hanani2019@gmail.com>
2022-04-25 10:19:03 -0400

    resolving shellcheck warning

--- setup.sh
+++ setup.sh
@@ -5,7 +5,7 @@ echo "Cloning project into anti-spoofing-rPPG"
 git clone "https://github.com/mhannani/anti-spoofing-rPPG"
 
 echo "getting into anti_spoofing_rPPG"
-cd anti-spoofing-rPPG
+cd anti-spoofing-rPPG || exit
 
 echo "Creating conda environment..."
 conda create --file environment.yml
